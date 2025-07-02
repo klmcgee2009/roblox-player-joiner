dde8791806a16039e3151265d896edefd8b83bc4
klmcgee2009 <149341927+klmcgee2009@users.noreply.github.com>
2025-07-01 22:07:40 -0600

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.action.onClicked.addListener(() => {
-  chrome.windows.create({
-    url: chrome.runtime.getURL("popup.html"),
-    type: "popup",
-    width: 450,
-    height: 400,
-    focused: true
-  });
-});
